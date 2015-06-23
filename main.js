ad97035fe2dff82abb12da2e4547088b3d3d2f70
samelie <sam@stinkdigital.com>
2015-06-23 19:00:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var CUT_OUT = [{
+	startTime:0
+	endTime:134
+},{
+	startTime:305
+	endTime:334
+}];
+
+
+//SEASON 1
+
+startTime:0
+endTime:134
+//MAIN
+startTime:362
+endTime:420
+//MAIN
+startTime:630
+endTime:730
+//MAIN
+startTime:937
+endTime:1010
+//CONCLUTION
+startTime:1260
+endTime:duration
